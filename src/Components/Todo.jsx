376e8060087d9b74742ea97d4dc55f8e9d0a6254
vivekds19 <dsvivek092@gmail.com>
2025-11-14 16:00:07 +0530

    Use separate states for current todo input and todos array

---
+++ src/Components/Todo.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useState } from "react";
+
+export default function Todo() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  function AddTodo(e) {
+    e.preventDefault();
+    setTodos([...todos, todo]);
+    setTodo("");
+  }
+  return (
+    <div>
+      <form>
+        <input
+          onChange={(e) => {
+            setTodo(e.target.value);
+          }}
+          value={todo}
+          type="text"
+        />
+        <button onClick={AddTodo} type="submit">
+          Add
+        </button>
+        <ul>
+          {todos.map((todoitems) => (
+            <li key={todoitems}>{todoitems}</li>
+          ))}
+        </ul>
+      </form>
+    </div>
+  );
+}
