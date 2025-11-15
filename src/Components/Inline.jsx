2b1d9cabb67f7337a3b19e63ddfb83a146a69ddf
vivekds19 <dsvivek092@gmail.com>
2025-11-15 11:03:20 +0530

    added the style to the header

--- src/Components/Inline.jsx
+++ src/Components/Inline.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import "../CSS/Style.css";
+import style from "./Inline.module.css";
 
 export default function Inline() {
   return (
     <div>
-      <h1 className="header">To-DO List</h1>
+      <h1 className={style.header}>To-DO List</h1>
     </div>
   );
 }
