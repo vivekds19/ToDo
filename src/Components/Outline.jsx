90c3aeac63e878d0a7b0938402a83cffdc61cdf4
vivekds19 <dsvivek092@gmail.com>
2025-11-15 11:02:37 +0530

    Created the outline Component

---
+++ src/Components/Outline.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './Outline.module.css'
+
+export default function Outline() {
+  return (
+    <div>
+      <h1 className={style.header}>This is a Outline Component</h1>
+    </div>
+  )
+}
