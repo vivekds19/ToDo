357c97d9c181dbbe35f24fc591028a3d71ba8e37
vivekds19 <dsvivek092@gmail.com>
2025-11-15 11:02:57 +0530

    added the outline component to the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Todo from "./Components/Todo";
 import Inline from "./Components/Inline";
+import Outline from "./Components/Outline";
 
 function App() {
   return (
     <div className="App">
       {/* <Todo /> */}
       <Inline />
+      <Outline />
     </div>
   );
 }
